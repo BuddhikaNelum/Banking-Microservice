f24a00916cea88bf3ea8d5d623247be144431fad
BuddhikaNelum <dlbnprabath@gmail.com>
2019-10-13 23:32:22 +0530

    Implemented publish and send command for banking microservice

---
+++ MicroRabbit.Banking.Domain/Events/TransferCreatedEvent.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Domain.Events
+{
+    public class TransferCreatedEvent : Event
+    {
+        public int From { get; private set; }
+        public int TO { get; private set; }
+        public decimal Amount { get; private set; }
+
+        public TransferCreatedEvent(int from, int to, decimal amount)
+        {
+            From = from;
+            TO = to;
+            Amount = amount;
+        }
+    }
+}
